dd41cc901b4e3677b1b3be860636db634ce45fed
Bubleks <gugugaga23166@gmail.com>
2025-09-26 12:44:09 +0500

    new

--- DJANGO_Project/DJANGO_App/admin.py
+++ DJANGO_Project/DJANGO_App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Title, Genre, Category, Platform, Catalogue
+
+admin.site.register(Title)
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Platform)
+admin.site.register(Catalogue)
--- DJANGO_Project/DJANGO_App/models.py
+++ DJANGO_Project/DJANGO_App/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Title(models.Model):
+    title_name = models.CharField("Название", max_length=50)
+
+class Genre(models.Model):
+    genre_name = models.CharField("Жанр", max_length=25)
+
+class Category(models.Model):
+    category = models.CharField("Категория", max_length=25)
+
+class Platform(models.Model):
+    platform = models.CharField("Платформа", max_length=15)
+
+class Catalogue(models.Model):
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name= 'название',
+        related_name= 'title'
+    )
+
+    genre_id = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_DEFAULT,
+        default=1,
+        verbose_name= 'жанр',
+        related_name= 'genre'
+    )
+    
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name='категории',
+        related_name='categories'
+    )
+
+    platforms = models.ManyToManyField(
+        Platform,
+        verbose_name='платформы',
+        related_name='platforms'
+    )
+    publish_date = models.DateField("Дата выхода", null=True)
+
+
