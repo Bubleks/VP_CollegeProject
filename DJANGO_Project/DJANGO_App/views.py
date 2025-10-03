4c2b90757e07dd0d49b85cc51889460d2ce9951d
Bubleks <gugugaga23166@gmail.com>
2025-10-03 11:24:53 +0500

    new

--- DJANGO_Project/DJANGO_App/views.py
+++ DJANGO_Project/DJANGO_App/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def print_hello(request):
+    return HttpResponse(request.method)
+
+#def print_hello(request):
+#    first = 10
+#    second = 15
+#    summary = first + second
+#    return HttpResponse(request.GET[])
--- DJANGO_Project/DJANGO_Project/urls.py
+++ DJANGO_Project/DJANGO_Project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DJANGO_App.views import print_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', print_hello),
 ]
