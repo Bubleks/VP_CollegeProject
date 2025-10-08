098355b77c42632771ad9747ae62d7815c590450
Bubleks <gugugaga23166@gmail.com>
2025-10-08 12:25:45 +0500

    new

--- DJANGO_Project/DJANGO_App/views.py
+++ DJANGO_Project/DJANGO_App/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from .models import Title
+
 
 def print_hello(request):
     return HttpResponse(request.method)
+
+def test_templates(request):
+    titles = Title.object.all()
+    return render(request, 'test.html',
+                  {
+                      'my_titles': titles
+                  })
 
 #def print_hello(request):
 #    first = 10
