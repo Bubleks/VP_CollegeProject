83faf226ae1d0d627c3f5c0dbbdc309043b59bba
Bubleks <gugugaga23166@gmail.com>
2025-10-10 11:21:17 +0500

    VP_item_html_taks_10:10:2025

--- DJANGO_Project/DJANGO_App/views.py
+++ DJANGO_Project/DJANGO_App/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, HttpResponse
-from .models import Title
+from .models import Title, Item
 
 
 def print_hello(request):
     return HttpResponse(request.method)
 
+#def test_templates(request):
+#    titles = Title.objects.all()
+#    return render(request, 'test.html',
+#                  {
+#                      'titles': titles
+#                  })
+
 def test_templates(request):
-    titles = Title.objects.all()
     return render(request, 'test.html',
-                  {
-                      'titles': titles
-                  })
+        {
+            'items': Item.objects.filter(price__lt=50)
+        })
 
 #def print_hello(request):
 #    first = 10
