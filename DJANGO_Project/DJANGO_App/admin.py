cca9dbceb07462608e3b31e52c46a927c7919469
Bubleks <gugugaga23166@gmail.com>
2025-10-10 11:04:35 +0500

    VP_task_10:10:2025

--- DJANGO_Project/DJANGO_App/admin.py
+++ DJANGO_Project/DJANGO_App/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from .models import Title, Genre, Category, Platform, Catalogue
+from .models import Title, Genre, Category, Platform, Catalogue, Item, Supplier, Shipping, Order
 
 admin.site.register(Title)
 admin.site.register(Genre)
 admin.site.register(Category)
 admin.site.register(Platform)
 admin.site.register(Catalogue)
+
+#
+#
+#
+
+admin.site.register(Item)
+admin.site.register(Supplier)
+admin.site.register(Shipping)
+admin.site.register(Order)
